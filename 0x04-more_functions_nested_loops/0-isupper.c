bc768c431479da4877c2f93b3e73831a47e2af21
ADEDAY <adedaygnigus@gmail.com>
2023-01-03 10:42:30 -0800

    0-isupper.c

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _isupper - checks for uppercase character
+ *  @c: Variable text
+ *  Return: Always 0.
+ */
+{
+
+	 if (c >= 'A' && c <= 'Z')
+	 {
+		 return (1);
+	 }
+	  else
+	  {
+		  return (0);
+	  }
+}
